cb7fbe7f532ca8faab0249bdefea67aa99b05ed4
Robert Österberg <rosterberg93@gmail.com>
2026-04-09 23:24:32 +0200

    Found target value

--- EPlan.XmlParse/Program.cs
+++ EPlan.XmlParse/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System.Xml.Linq;
 
-XDocument XmlFile =  
+XDocument xdoc = XDocument.Load("XMLFile1.xml");
+
+string result = xdoc.Descendants("trans-unit")
+    .Where(x => x.Attribute("id").Value == "42014")
+    .First()
+    .Element("target").Value;
+
+Console.WriteLine(result);
 
